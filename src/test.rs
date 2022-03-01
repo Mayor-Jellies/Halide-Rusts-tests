d3ffb94d774f2b25b8e848fc886434a49fa27836
James O'Reilly <James.oreilly@mnsu.edu>
2022-03-01 10:41:56 -0600

    fix intenially failing test
    Signed-off-by: James O'Reilly <James.oreilly@mnsu.edu>

--- src/test.rs
+++ src/test.rs
@@ -8,7 +8,7 @@ mod tests {
     use crate::HalideRuntime::halide_malloc;
     #[test]
     fn i_got_here() {
-        assert_eq!(2 + 2, 5);
+        assert_eq!(2 + 2, 4);
     }
     #[test]
     fn memalloc(){
