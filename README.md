b2934a023eb5d0ad0b5d71ffdc5e2e4909e799ac
rootbutcher2 <james.oreilly@mnsu.edu>
2022-04-05 17:46:52 -0500

    NON-funtional!!!! in progress automatic build

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Halide-Rusts-tests
+
+##Description of pruduct
+
+
+##Demo app Getting Started Guild
+1.) Install Halide Source
+    a.) The following install guild should be used: (link to halide install guild)
+2.)
+    install rust Following the below install guild:
+        link to rust install guild
+    install Rust Cargo
+        link to cargo install guild
+3.)
+    1.) In the termianl cd into the Halide/RustBindings/Demo
+    2.) run Cargo Build
+    3.) run Cargo test
+    4.) run Cargo run
+
+4.)
--- build.rs
+++ build.rs
@@ -5,8 +5,39 @@ use std::env;
 use std::path::Path;
 
 fn main() {
+
+    let Halide_path = Path::new("/Halide");
+
+    cc::Build::new()
+        .cpp(true)
+        .cargo_metadata(false)
+        //.cpp_link_stdlib("c++")
+        .flag("-Wno-all")
+        .debug(true)
+        .flag("-Wno-unused-parameter")
+        .include("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/include")
+        .include("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/tools")
+        .object("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/lib/libHalide.so")
+        //.object("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/apps/iir_blur/iir_blur_generator.o")
+        .flag("-lHalide")
+        .flag("-lpthread")
+        .flag("-ldl")
+        .file()
+        .file("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/apps/iir_blur/iir_blur_generator.cpp")
+        .out_dir("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests")
+        .compile("iir_blur_test.generator");
+
     //RUSTFLAGS="-Z sanitizer=address";
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("cargo:rustc-link-search=native={}", Path::new(&dir).display());
 }
+/*
+system(c lib)
+std::process::comand(path, argv)
+--help
 
+g++ -O3 -std=c++17 -I /home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/include/ -I /home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/tools/  -Wall -Werror -Wno-unused-function -Wcast-qual -Wignored-qualifiers -Wno-comment -Wsign-compare -Wno-unknown-warning-option -Wno-psabi -g iir_blur_generator.cpp /home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/tools/GenGen.cpp -o bin/host/iir_blur.generator -Wl,-rpath,/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/lib/ -L /home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/lib/ -lHalide -ldl -lpthread -lz -Wl,--no-as-needed -lautoschedule_mullapudi2016 -Wl,--as-needed
+bin/host/iir_blur.generator -g iir_blur -f iir_blur -o bin/host target=host-no_runtime auto_schedule=false
+running: "cc" "-O0" "-ffunction-sections" "-fdata-sections" "-fPIC" "-g" "-fno-omit-frame-pointer" "-m64" "-I" "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/include/" "-I" "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/tools/" "-Wall" "-Wextra" "-Wall" "-Wno-unused-function" "-Wcast-qual" "-Wignored-qualifiers" "-o" "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/apps/iir_blur/iir_blur_generator.cpp /home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/tools/GenGen.o" "-c" "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/apps/iir_blur/iir_blur_generator.cpp /home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/tools/GenGen.cpp"
+
+*/
